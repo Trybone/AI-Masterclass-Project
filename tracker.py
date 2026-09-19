0071418b89617d1f20091c5475ae3fa3f0cbab97
Brightone Salamba <salambabrightone@gmail.com>
2026-09-19 11:18:38 +0100

    Add daily step trend calculation

--- tracker.py
+++ tracker.py
@@ -1,11 +1,7 @@
 # Testing git diff
 # Testing finished.
-<<<<<<< HEAD
-def assess_day(steps):
-    return steps >= 12000
-=======
-
 
 def assess_day(steps, goal=10000):
     return steps >= goal
->>>>>>> feature/flexible-goal
+
+# Target steps this week 10500
